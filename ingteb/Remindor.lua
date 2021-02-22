05668b9872daa57657544d8bab525d777a60ea8a
hahoyer.Anja <hahoyer@gmail.com>
2021-02-22 22:14:46 +0100

    - easier remindor settings

--- ingteb/Remindor.lua
+++ ingteb/Remindor.lua
@@ -18,29 +18,19 @@ local Class = class:new(
         Database = {get = function(self) return self.Parent.Database end},
         AutoResearch = {
             get = function(self)
-                if self.Settings.AutoResearch ~= nil then
-                    return self.Settings.AutoResearch
-                end
                 return self.ParentData.Settings.AutoResearch
             end,
         },
         AutoCrafting = {
             get = function(self)
-                if self.Settings.AutoCrafting ~= nil then
-                    return self.Settings.AutoCrafting
-                end
                 return self.ParentData.Settings.AutoCrafting
             end,
         },
         RemoveTaskWhenFulfilled = {
             get = function(self)
-                if self.Settings.RemoveTaskWhenFulfilled ~= nil then
-                    return self.Settings.RemoveTaskWhenFulfilled
-                end
                 return self.ParentData.Settings.RemoveTaskWhenFulfilled
             end,
         },
-        Settings = {get = function(self) return self.Global.Remindor.Settings end},
         HelperTextSettings = {
             get = function(self)
                 local result = Array:new{}
@@ -87,36 +77,9 @@ function Class:new(parent)
     return self
 end
 
-function Class:CloseSettings()
-    if not self.CurrentSettings then return end
-    self.CurrentSettings.destroy()
-    if self.ParentScreen then
-        self.ParentScreen.ignored_by_interaction = nil
-        self.Player.opened = self.ParentScreen
-        self.ParentScreen = nil
-
-    end
-    self.CurrentSettings = nil
-end
-
-function Class:OpenSettings(target)
-    self:CloseSettings()
-    self.CurrentSettings = Settings.Open(self, target or self)
-end
-
-function Class:RefreshSettings(target)
-    self.CurrentSettings.destroy()
-    if self.ParentScreen and self.ParentScreen.valid then
-        self.ParentScreen.ignored_by_interaction = nil
-        self.Player.opened = self.ParentScreen
-    end
-    self.CurrentSettings = Settings.Open(self, target or self)
-end
-
 function Class:RestoreFromSave(parent)
     self.Parent = parent
     if not self.Global.Remindor then self.Global.Remindor = {} end
-    if not self.Global.Remindor.Settings then self.Global.Remindor.Settings = {} end
     local current = mod_gui.get_frame_flow(self.Player)[self.class.name]
     local list = self.Global.Remindor.List or {}
     self.Global.Remindor.List = Array:new()
@@ -129,20 +92,11 @@ function Class:RestoreFromSave(parent)
         self:Open()
     end
     self:Refresh()
-
-    local currentSettings = self.Player.gui.screen.RemindorSettings
-    if currentSettings then
-        local taskIndex = self:GetTaskIndex(currentSettings.children[2].name)
-        local target = taskIndex and self.Global.Remindor.List[taskIndex] or self
-        currentSettings.destroy()
-        self:OpenSettings(target)
-    end
 end
 
 function Class:Toggle()
     if self.Current then
         self:Close()
-        self:CloseSettings()
     else
         self:Open()
         self:Refresh()
@@ -166,26 +120,14 @@ function Class:Open()
             direction = "vertical",
             style_mods = {horizontally_stretchable = "on"},
         }, --
-        {"ingteb-utility.reminder-tasks"}, --
-        {
-            buttons = {
-                {
-                    type = "sprite-button",
-                    sprite = "ingteb_settings_white",
-                    style = "frame_action_button",
-                    actions = {on_click = {module = self.class.name, action = "Settings"}},
-                    tooltip = self.HelperTextSettings,
-                },
-            },
-        }
+        {"ingteb-utility.reminder-tasks"} --
     )
     self.Tasks = result.Tasks
     self.Current = result.Main
     self:Refresh()
 end
 
-function Class:Reopen(target)
-    if self.CurrentSettings then self:RefreshSettings(target) end
+function Class:Reopen()
     if self.Current then
         self:Close()
         self:Open()
@@ -245,19 +187,22 @@ function Class:OnGuiEvent(event)
     local target = taskIndex and self.Global.Remindor.List[taskIndex] or self
 
     if message.action == "Update" then
-        target.Settings[message.control] = self:GetValueOfControl(event.element)
-        self:Reopen(target)
+        dassert(target == self)
+        self.Settings[message.control] = self:GetValueOfControl(event.element)
+        self:Reopen()
     elseif message.action == "UpdateOverride" then
+        dassert(target == self)
         if event.element.state then
-            target.Settings[message.control] = nil
+            self.Settings[message.control] = nil
         else
-            target.Settings[message.control] = target[message.control]
+            self.Settings[message.control] = self[message.control]
         end
-        self:Reopen(target)
+        self:Reopen()
     elseif message.action == "Click" then
         self.Parent:OnGuiClick(event)
     elseif message.action == "Settings" then
-        self:OpenSettings(target)
+        dassert(target == self)
+        self:OpenSettings()
     elseif message.action == "SettingsClick" then
         if message.target == "SelectRemindor" then
             self.Parent.Modules.SelectRemindor.Settings:OnClick(event)
--- ingteb/RemindorSettings.lua
+++ ingteb/RemindorSettings.lua
@@ -19,91 +19,6 @@ local Class = class:new(
     }
 )
 
-local function GetGuiForDefaultCheckBox(self, controlName)
-    return {
-        type = "checkbox",
-        caption = {"ingteb-utility.default"},
-        state = self.Settings[controlName] == nil,
-        actions = {
-            on_checked_state_changed = {
-                module = "Remindor",
-                target = self.class.name,
-                action = "UpdateOverride",
-                control = controlName,
-                key = self.CommonKey,
-            },
-        },
-    }
-end
-
-local function GetGuiForControl(self, controlName, caption, values, textOfValues)
-    local result = {
-        type = values and "drop-down" or "checkbox",
-        caption = caption,
-        ignored_by_interaction = self.Settings[controlName] == nil,
-        actions = {
-            on_checked_state_changed = {
-                module = "Remindor",
-                target = self.class.name,
-                action = "Update",
-                control = controlName,
-                key = self.CommonKey,
-            },
-            on_selection_state_changed = {
-                module = "Remindor",
-                target = self.class.name,
-                action = "Update",
-                control = controlName,
-                key = self.CommonKey,
-            },
-        },
-    }
-    if values then
-        result.items = textOfValues
-        result.selected_index = Array:new(values):IndexWhere(
-            function(value) return value == self[controlName] end
-        )
-    else
-        result.state = self[controlName]
-    end
-    return result
-end
-
-local function GetGuiForControlGroup(self, controlName, caption, values, textOfValues)
-    if self[controlName] == nil then return {} end
-    return {
-        {
-            type = "flow",
-            direction = "horizontal",
-            children = {
-                GetGuiForDefaultCheckBox(self, controlName),
-                GetGuiForControl(self, controlName, caption, values, textOfValues),
-            },
-        },
-    }
-end
-
-local function GetGui(self)
-
-    local children = Array:new{
-        GetGuiForControlGroup(self, "AutoResearch", {"ingteb-utility.auto-research"}),
-        GetGuiForControlGroup(
-            self, "AutoCrafting", {"ingteb-utility.auto-crafting"}, Constants.AutoCraftingVariants,
-                {
-                    {"string-mod-setting.ingteb_reminder-task-autocrafting-off"},
-                    {"string-mod-setting.ingteb_reminder-task-autocrafting-1"},
-                    {"string-mod-setting.ingteb_reminder-task-autocrafting-5"},
-                    {"string-mod-setting.ingteb_reminder-task-autocrafting-all"},
-                }
-        ),
-        GetGuiForControlGroup(
-            self, "RemoveTaskWhenFulfilled", {"ingteb-utility.remove-when-fulfilled"}
-        ),
-    }:ConcatMany()
-
-    return {type = "flow", direction = "vertical", name = self.CommonKey, children = children}
-end
-
 function Class:GetHelp(tag)
     local localisedNames = {
         AutoResearch = "ingteb-utility.select-remindor-autoresearch-help",
@@ -241,25 +156,5 @@ function Class:GetGui()
 end
 
 function Class:new(parent, parameters) return self:adopt{Parent = parent, Parameters = parameters} end
-
-function Class.Open(remindor, self)
-    if not self.Global.Location.RemindorSettings then
-        self.Global.Location.RemindorSettings = {x = 200, y = 100}
-    end
-    local caption = {"ingteb-utility.reminder-tasks-settings"}
-    if self.class.name == "Task" then
-        caption = {
-            "",
-            caption,
-            self.Target.RichTextName,
-            self.Worker.RichTextName,
-            self.Recipe.RichTextName,
-        }
-    end
-    local result = Helper.CreatePopupFrameWithContent(
-        remindor, GetGui(self), caption, {subModule = "Settings"}
-    )
-    return result.Main
-end
 
 return Class
