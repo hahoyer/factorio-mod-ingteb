5cfe51df029e3c9917dcae36cf905b3b25ff6b05
hh <hahoyer@gmail.com>
2022-04-19 21:48:53 +0200

    change: Entity class uses backlinks to get its categories

--- ingteb/Entity.lua
+++ ingteb/Entity.lua
@@ -27,7 +27,7 @@ Class.system.Properties = {
         get = function(self)
             local place = self.Prototype.items_to_place_this
             if not place or #place == 0 then return end
-            -- todo: present multiple items_to_place_this 
+            -- todo: present multiple items_to_place_this
             return self.Database:GetItem(place[1].name)
         end,
     },
@@ -37,7 +37,7 @@ Class.system.Properties = {
         get = function(self)
             local prototype = self.Prototype
             if not prototype.mineable_properties --
-            or not prototype.mineable_properties.minable --
+                or not prototype.mineable_properties.minable --
                 or not prototype.mineable_properties.products --
             then return end
             return not prototype.items_to_place_this
@@ -49,9 +49,9 @@ Class.system.Properties = {
         cache = true,
         get = function(self)
             local data = self.Prototype.electric_energy_source_prototype
-                             or self.Prototype.fluid_energy_source_prototype
-                             or self.Prototype.heat_energy_source_prototype
-                             or self.Prototype.void_energy_source_prototype
+                or self.Prototype.fluid_energy_source_prototype
+                or self.Prototype.heat_energy_source_prototype
+                or self.Prototype.void_energy_source_prototype
             if data then --
                 return --
             end
@@ -61,29 +61,29 @@ Class.system.Properties = {
     FuelCategories = {
         cache = true,
         get = function(self)
-            return self.Categories --
-            :Where(
-                function(category)
-                    return category.Domain == "burning" or category.Domain == "fluid-burning"
-                end
-            ) --
-            :ToArray(
-                function(category)
-                    local name = category.Domain == "fluid-burning" and "fluid" or category.SubName
-                    return self.Database:GetFuelCategory(name)
-                end
-            )
+            return self.Categories--
+                :Where(
+                    function(category)
+                        return category.Domain == "burning" or category.Domain == "fluid-burning"
+                    end
+                )--
+                :ToArray(
+                    function(category)
+                        local name = category.Domain == "fluid-burning" and "fluid" or category.SubName
+                        return self.Database:GetFuelCategory(name)
+                    end
+                )
         end,
     },
 
     UsefulLinks = {
         cache = true,
         get = function(self)
-            return Array:new{
+            return Array:new {
                 self.FuelCategories,
-                self.Modules --
-                :Where(function(value) return value end) --
-                :ToArray(function(_, module) return module end),
+                self.Modules--
+                    :Where(function(value) return value end)--
+                    :ToArray(function(_, module) return module end),
             }
         end,
     },
@@ -91,17 +91,15 @@ Class.system.Properties = {
     Categories = {
         cache = true,
         get = function(self)
-            local xreturn = self.Database.Proxies.Category -- 
-            :Where(
-                function(category)
-                    local workers = self.Database.BackLinks.WorkersForCategory[category.Name]
-                    return workers
-                               and workers:Any(function(worker)
-                            return worker == self.Prototype
-                        end)
-                end
-            ) --
-
+            local xreturn = Dictionary:new(self.Database.BackLinks.WorkersForCategory)--
+                :Where(
+                    function(workers)
+                        return workers and workers:Any(
+                            function(_, name) return name == self.Prototype.name end
+                        )
+                    end
+                )--
+                :Select(function(_, categoryName) return self.Database:GetCategory(categoryName) end)
             return xreturn
         end,
     },
@@ -109,9 +107,9 @@ Class.system.Properties = {
     RecipeList = {
         cache = true,
         get = function(self)
-            return self.Categories --
-            :Select(function(category) return category.RecipeList end) --
-            :Where(function(recipes) return recipes:Any() end) --
+            return self.Categories--
+                :Select(function(category) return category.RecipeList end)--
+                :Where(function(recipes) return recipes:Any() end) --
         end,
     },
 
@@ -119,23 +117,23 @@ Class.system.Properties = {
         get = function(self)
             local inherited = self.inherited.Entity.SpecialFunctions.get(self)
             if not self.Item then return inherited end
-            local result = Array:new{
+            local result = Array:new {
                 {
                     UICode = "--- l", --
-                    Action = function(self) return {Presenting = self.Item} end,
+                    Action = function(self) return { Presenting = self.Item } end,
                 },
                 {
                     UICode = "-C- l",
                     HelpText = "controls.smart-pipette",
                     Action = function(self)
-                        return {Selecting = self.Item, Entity = self}
+                        return { Selecting = self.Item, Entity = self }
                     end,
                 },
                 {
                     UICode = "--- r",
-                    IsRestricedTo = {Presentator = true, Remindor = true},
+                    IsRestricedTo = { Presentator = true, Remindor = true },
                     HelpText = "ingteb-utility.create-reminder-task",
-                    Action = function(self) return {RemindorTask = self.Item} end,
+                    Action = function(self) return { RemindorTask = self.Item } end,
                 },
             }
             return result:Concat(inherited)
@@ -147,7 +145,7 @@ Class.system.Properties = {
         get = function(self)
             local prototype = self.Prototype
             return prototype.type == "furnace" or prototype.type == "rocket-silo"
-                       or prototype.fixed_recipe ~= nil
+                or prototype.fixed_recipe ~= nil
         end,
     },
 
@@ -163,8 +161,8 @@ Class.system.Properties = {
         cache = true,
         get = function(self) --
             local result = 60.0
-                               / (self.Prototype.rocket_rising_delay
-                                   + self.Prototype.launch_wait_time)
+                / (self.Prototype.rocket_rising_delay
+                    + self.Prototype.launch_wait_time)
             dassert(result and result > 0)
             return result
         end,
@@ -227,10 +225,10 @@ function Class:new(name, prototype, database)
 
     -- ConditionalBreak(self.Prototype.target_temperature)
 
-    self.UsedBy = Dictionary:new{}
-    self.CreatedBy = Dictionary:new{}
-    self.Amounts = {value = 1}
-    self.HelpTextWhenUsedAsProduct = {"", self.RichTextName .. " ", self.Prototype.localised_name}
+    self.UsedBy = Dictionary:new {}
+    self.CreatedBy = Dictionary:new {}
+    self.Amounts = { value = 1 }
+    self.HelpTextWhenUsedAsProduct = { "", self.RichTextName .. " ", self.Prototype.localised_name }
 
     if self.IsResource then
         self.TypeStringForLocalisation = "ingteb-utility.title-resource"
